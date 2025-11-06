d05e7b9d8a34a9963fc674de9f392d5f52d5f34e
jjuidev <jjuidev@gmail.com>
2025-11-06 22:07:18 +0700

    feat: ✨ linter with @jjuidev/node-devtools

---
+++ .commitlintrc.cjs
@@ -0,0 +1,3 @@
+const { commitlintConfigRecommend } = require('@jjuidev/node-devtools');
+
+module.exports = commitlintConfigRecommend;
---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import { defineEslintConfig, eslintConfigNode } from '@jjuidev/node-devtools';
+
+const eslintConfig = defineEslintConfig(eslintConfigNode);
+
+export default eslintConfig;
